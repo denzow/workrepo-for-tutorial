0ab14797d1e4b379c93b0324a8711592d96f644e
denzow <denzow@gmail.com>
2018-09-18 15:52:29 +0900

    [add] header

--- application/views/urls.py
+++ application/views/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
 
-    path('api/accounts', AccountsApi.as_view())
+    path('api/accounts/', AccountsApi.as_view())
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
