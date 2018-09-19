9730eeca7dcd7a72f10af327647d9234eedee492
denzow <denzow@gmail.com>
2018-09-19 22:39:01 +0900

    [wip]

---
+++ application/modules/kanban/models/board.py
@@ -0,0 +1,13 @@
+from django.conf import settings
+from django.db import models
+
+
+class Board(models.Model):
+
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return '{}: {} of {}'.format(self.pk, self.name, self.owner)
